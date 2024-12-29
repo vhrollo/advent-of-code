4290e5a59f77d140399ae60c6cefd5f86064bb7b
vhrollo <103120448+vhrollo@users.noreply.github.com>
2024-12-29 01:54:32 +0100

    13 ended up being pretty cool

---
+++ 2024/day13/13.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+M = np.fromregex('test.txt', r'\d+', [('', int)]
+    *6).view(int).reshape(-1,3,2).swapaxes(1,2)
+
+S = np.kron(np.eye(2), M[..., :2])
+P = np.concatenate([M[..., 2:], M[..., 2:]+1e13], 1)
+R = np.linalg.solve(S, P).round().astype(np.int64)
+print(*np.sum((R.reshape(-1,2,2) @ [3,1]) * (S @ R == P).reshape(-1,2,2).all(2), 0))
