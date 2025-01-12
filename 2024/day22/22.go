b5e94cf0fd4ca34a83d807b6e4ed071687cf7c3d
Fillip Lampe <fillip@dlampe.com>
2025-01-12 15:20:03 +0100

    fix

--- 2024/day22/22.go
+++ 2024/day22/22.go
@@ -1,14 +1,11 @@
-// fun task to solve
-
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "strconv"
-    "sync"
-	"time"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"sync"
 )
 
 const magic = 0x1000000 // 16777216
@@ -18,62 +15,62 @@ type sequence [4]int
 var memoSeq sync.Map
 
 func getData() []int {
-    dat, _ := os.ReadFile("input.txt")
-    lines := strings.Split(strings.TrimSpace(string(dat)), "\n")
-    intLines := make([]int, len(lines))
-    for i, line := range lines {
-        intLines[i], _ = strconv.Atoi(line)
-    }
-    return intLines
+	dat, _ := os.ReadFile("input.txt")
+	lines := strings.Split(strings.TrimSpace(string(dat)), "\n")
+	intLines := make([]int, len(lines))
+	for i, line := range lines {
+		intLines[i], _ = strconv.Atoi(line)
+	}
+	return intLines
 }
 
 func step(S int) int {
-    S = (S ^ (S <<  6)) % magic
+	S = (S ^ (S <<  6)) % magic
 	S = (S ^ (S >>  5)) % magic
 	S = (S ^ (S << 11)) % magic
-    return S
+	return S
 }
 
 func part1(data []int) {
-    total := 0
-    for _, val := range data {
-        for i := 0; i < 2000; i++ {
-            val = step(val)
-        }
-        total += val
-    }
+	total := 0
+	for _, val := range data {
+		for i := 0; i < 2000; i++ {
+			val = step(val)
+		}
+		total += val
+	}
 	fmt.Println("Part1:", total)
 }
 
 func generatePricesAndChanges(initial int, n int) ([]int, []int) {
-    prices := make([]int, n)
-    changes := make([]int, n)
-    for i := 0; i < n; i++ {
-        prices[i] = initial % 10
-        initial = step(initial)
-        if i == 0 {
-            changes[i] = 0
-        } else {
-            changes[i] = prices[i] - prices[i-1]
-        }
-    }
-    return prices, changes
+	prices := make([]int, n)
+	changes := make([]int, n)
+	for i := 0; i < n; i++ {
+		prices[i] = initial % 10
+		initial = step(initial)
+		if i == 0 {
+			changes[i] = 0
+		} else {
+			changes[i] = prices[i] - prices[i-1]
+		}
+	}
+	return prices, changes
 }
 
 func bestSequence(prices, changes []int) map[sequence]int {
-    localMap := make(map[sequence]int)
-    for i := 4; i < len(changes); i++ {
-        seq := sequence{
-            changes[i-3], 
-            changes[i-2], 
-            changes[i-1], 
-            changes[i],
-        }
+	localMap := make(map[sequence]int)
+	for i := 4; i < len(changes); i++ {
+		seq := sequence{
+			changes[i-3], 
+			changes[i-2], 
+			changes[i-1], 
+			changes[i],
+		}
 		// the earliest occurrence of a sequence is the will be taken
-        if _, exists := localMap[seq]; !exists {
-            localMap[seq] = prices[i]
-        }
-    }
+		if _, exists := localMap[seq]; !exists {
+			localMap[seq] = prices[i]
+		}
+	}
 
 	return localMap
 }
@@ -82,7 +79,7 @@ func part2(data []int) {
 	globalMap := make(map[sequence]int)
 
 	var wg sync.WaitGroup
-    localMaps := make(chan map[sequence]int, len(data))
+	localMaps := make(chan map[sequence]int, len(data))
 	
 	for _, val := range data {
 		wg.Add(1)
@@ -104,21 +101,19 @@ func part2(data []int) {
 		}
 	}
 
-    highest := 0
-    var bestSeq sequence
-    for k, sumVal := range globalMap {
-        if sumVal > highest {
-            highest = sumVal
-            bestSeq = k
-        }
-    }
-    fmt.Println("Part2:", highest, bestSeq)	
+	highest := 0
+	var bestSeq sequence
+	for k, sumVal := range globalMap {
+		if sumVal > highest {
+			highest = sumVal
+			bestSeq = k
+		}
+	}
+	fmt.Println("Part2:", highest, bestSeq)	
 }
 
 func main() {
-    data := getData()
+	data := getData()
 	part1(data)
-	start := time.Now()
-    part2(data)
-	fmt.Println("Execution time:", time.Since(start))
+	part2(data)
 }
